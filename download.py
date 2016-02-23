aa6d2c82a561579f24db859d721a4385d8ed554a
anna-wasewicz <wasewicz@umich.edu>
2016-02-23 02:39:08 -0500

    forgot download.py

--- download.py
+++ download.py
@@ -1,22 +1,20 @@
 from piazza_api import Piazza
-import pprint
 import json
+import sys
 
 p = Piazza()
 p.user_login()
 
-eecs183 = p.network("iiw41ofw7igeu")
+course = p.network(argv[1])
 
-mapSave={}
+mapSave = {}
 
-posts = eecs183.iter_all_posts(limit=100000000000)
+posts = course.iter_all_posts(limit=100000000000)
 for post in posts:
-	# print("type ",post["status"] )
+	content = post["history"][0]["content"]
+	id = post["nr"]
+	print id
+	mapSave[id] = content
 
-	if post["status"] != "private":
-		content=post["history"][0]["content"]
-		id=post["nr"]
-		mapSave[id]=content
-
-with open("eecs281jsim.json","wb") as f:
+with open("posts.json","wb") as f:
 	f.write(json.dumps(mapSave))
