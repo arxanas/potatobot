1874695a1cc5aedcc12d9fa68885f86135aa541a
Anna <wasewicz@umich.edu>
2016-02-24 04:56:01 +0000

    python 3 compatible per waleed's request

--- download.py
+++ download.py
@@ -13,7 +13,7 @@ posts = course.iter_all_posts(limit=100000000000)
 for post in posts:
 	content = post["history"][0]["content"]
 	id = post["nr"]
-	print id
+	print (id)
 	mapSave[id] = content
 
 with open("posts183.json","wb") as f:
